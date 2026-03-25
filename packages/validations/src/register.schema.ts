ff0cb59fa58c465b9aab9c69d53e382fc6b85594
Arun <arun.prabhakaran@skystack.io>
2026-03-25 12:35:08 +0530

    feat(web): add auth layouts and validation schemas

---
+++ packages/validations/src/register.schema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const registerSchema = z
+  .object({
+    name: z.string().min(1, "Name is required"),
+    email: z.email("Invalid email address").min(1, "Email is required"),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+    confirmPassword: z
+      .string()
+      .min(6, "Confirm Password must be at least 6 characters long"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export type RegisterSchemaInput = z.infer<typeof registerSchema>;
