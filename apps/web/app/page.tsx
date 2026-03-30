134f4a5325fb65a2cdf164f44ba7640a7bd9dd6b
Arun <arunprakashrani@gmail.com>
2026-03-30 19:42:41 +0530

    feat(web): implement dashboard modules and auth providers

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@repo/ui/components";
+import Home from "@/modules/dashboard/home"
 
 export default function Page() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24 bg-black">
-      <Button>hello world</Button>
+      <Home />
     </main>
-  );
+  )
 }
