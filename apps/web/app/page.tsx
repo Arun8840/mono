8ab5c94390de51239f3b5510f1eb7d5e2be98ecb
Arun <arun.prabhakaran@skystack.io>
2026-03-31 19:50:50 +0530

    chore: remove legacy task management logic and old migrations

--- apps/web/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Home from "@/modules/dashboard/home"
-
-export default function Page() {
-  return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24 bg-black">
-      <Home />
-    </main>
-  )
-}
--- packages/server/src/plugins/task.plugin.ts
+++
@@ -1,7 +0,0 @@
-import { Elysia } from "elysia"
-import { createTaskService } from "../service"
-
-export const taskPlugin = new Elysia({ name: "task-service-plugin" }).decorate(
-  "taskService",
-  createTaskService(),
-)
