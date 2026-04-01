30c9ffe3a8c635c0fada0c336496f6ec6ef22d68
Arun <arun.prabhakaran@skystack.io>
2026-04-01 13:38:53 +0530

    feat(validations): update application validation schemas

--- packages/validations/src/application.schema.ts
+++ packages/validations/src/application.schema.ts
@@ -1,22 +1,28 @@
-import z from "zod"
+import z from "zod";
 
 export const createApplicationSchema = z.object({
   title: z.string().min(3).max(100),
   description: z.string().min(3).max(1000),
-})
+});
 
-export type CreateApplicationInputType = z.infer<typeof createApplicationSchema>
+export type CreateApplicationInputType = z.infer<
+  typeof createApplicationSchema
+>;
 
 export const deleteApplicationSchema = z.object({
   appId: z.string(),
-})
+});
 
-export type DeleteApplicationInputType = z.infer<typeof deleteApplicationSchema>
+export type DeleteApplicationInputType = z.infer<
+  typeof deleteApplicationSchema
+>;
 
 export const udpateApplicationSchema = z.object({
   id: z.string(),
   title: z.string().min(3).max(100),
   description: z.string().min(3).max(1000),
-})
+});
 
-export type UpdateApplicationInputType = z.infer<typeof udpateApplicationSchema>
+export type UpdateApplicationInputType = z.infer<
+  typeof udpateApplicationSchema
+>;
