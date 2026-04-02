e0f8d253a69c6aec78ca8ee0768cda9393f9f3e5
arun <arunprakashrani@gmail.com>
2026-04-02 19:47:42 +0530

    feat(ui):popover component exported in main packages ui baralel export

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,20 +1,20 @@
 // UI Components
-export { Button } from "./ui/button";
-export { Input } from "./ui/input";
-export { Spinner } from "./ui/spinner";
-export { Separator } from "./ui/separator";
-export { Skeleton } from "./ui/skeleton";
+export { Button } from "./ui/button"
+export { Input } from "./ui/input"
+export { Spinner } from "./ui/spinner"
+export { Separator } from "./ui/separator"
+export { Skeleton } from "./ui/skeleton"
 export {
   Sheet,
   SheetContent,
   SheetHeader,
   SheetTitle,
   SheetDescription,
-} from "./ui/sheet";
-export { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
-export { Label } from "./ui/label";
-export { toast } from "sonner";
-export { Field, FieldError, FieldLabel, FieldGroup } from "./ui/field";
+} from "./ui/sheet"
+export { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip"
+export { Label } from "./ui/label"
+export { toast } from "sonner"
+export { Field, FieldError, FieldLabel, FieldGroup } from "./ui/field"
 export {
   Sidebar,
   SidebarContent,
@@ -40,7 +40,17 @@ export {
   SidebarSeparator,
   SidebarTrigger,
   useSidebar,
-} from "./ui/sidebar";
+} from "./ui/sidebar"
 
-export { ModalDrawer } from "./ui/Custom-modal";
-export { NuqsAdapterProvider } from "./ui/Nuqs-adapter";
+export { ModalDrawer } from "./ui/Custom-modal"
+export { NuqsAdapterProvider } from "./ui/Nuqs-adapter"
+export {
+  Popover,
+  PopoverContent,
+  PopoverDescription,
+  PopoverHeader,
+  PopoverTitle,
+  PopoverTrigger,
+} from "./ui/popover"
+
+export { Draggable, Droppable, DndProvider } from "./ui/dnd"
