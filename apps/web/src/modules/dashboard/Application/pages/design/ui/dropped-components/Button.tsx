5ba35322de365b7bec485e9e5b406a2544967a5f
Arun <arun.prabhakaran@skystack.io>
2026-04-08 17:30:39 +0530

    feat(web): add Button component to builder registry

---
+++ apps/web/src/modules/dashboard/Application/pages/design/ui/dropped-components/Button.tsx
@@ -0,0 +1,60 @@
+import React from "react"
+import DroppedComponentWrapper from "../component-wrapper"
+import { ComponentWrapperProps } from "@/types/comp.wrapper.types"
+import { Button } from "@repo/ui/components"
+import { EditorContent, useEditor } from "@tiptap/react"
+import StarterKit from "@tiptap/starter-kit"
+import TextAlign from "@tiptap/extension-text-align"
+import { useMutation } from "@tanstack/react-query"
+import { useApplicationStore } from "@/store/app"
+import { client } from "@repo/server/client"
+
+export interface DroppedButtonProps extends ComponentWrapperProps {}
+const DroppedButton: React.FC<DroppedButtonProps> = ({
+  value,
+  dimensions,
+  isPreview,
+}) => {
+  const { properties } = value
+  const content = properties?.content || "<p>Button</p>"
+  const updateComponent = useApplicationStore((state) => state?.updateComponent)
+  const updateComponentMutate = useMutation({
+    mutationFn: async () => {
+      await client.app.pages.component.page.component.update.post(updateReq)
+    },
+  })
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+    ],
+    content: content,
+    immediatelyRender: false,
+    onBlur: async () => {
+      updateComponent?.(updateReq)
+      updateComponentMutate?.mutate()
+    },
+    editable: !isPreview,
+  })
+  const updateReq = {
+    ...value,
+    properties: {
+      content: editor?.getHTML(),
+    },
+  }
+  return (
+    <DroppedComponentWrapper
+      value={value}
+      dimensions={dimensions}
+      isPreview={isPreview}
+    >
+      <Button className={"w-full h-full"}>
+        <EditorContent editor={editor} className="outline-none" />
+      </Button>
+    </DroppedComponentWrapper>
+  )
+}
+
+export default DroppedButton
